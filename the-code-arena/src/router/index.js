8f2afef2021bfdde2778b0325c953d7a5ace52d4
Luisa Ramos Barros <luisa.barros@s2tech.dev>
2026-03-27 14:20:39 -0300

    página única de landing page

--- the-code-arena/src/router/index.js
+++ the-code-arena/src/router/index.js
@@ -1,10 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../views/LandingPage.vue'
-import LoginPage from '../views/LoginPage.vue'
-import HomePage from '../views/HomePage.vue'
-import MedalPage from '../views/MedalPage.vue'
-import TeamsPage from '../views/TeamsPage.vue'
-import EventsPage from '../views/EventsPage.vue'
 
 const routes = [
     {
@@ -12,31 +7,6 @@ const routes = [
         name: 'landing',
         component: LandingPage
     },
-    {
-        path: '/login',
-        name: 'login',
-        component: LoginPage
-    },
-    {
-        path: '/home',
-        name: 'home',
-        component: HomePage
-    },
-    {
-        path: '/medals',
-        name: 'medals',
-        component: MedalPage
-    },
-    {
-        path: '/teams',
-        name: 'teams',
-        component: TeamsPage
-    },
-    {
-        path: '/events',
-        name: 'events',
-        component: EventsPage
-    }
 ]
 
 const router = createRouter({
